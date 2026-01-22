ae2d4be5d8496b7f3f7ca98db41a6afd1833af8d
SunniM <sunnismagan@gmail.com>
2026-01-22 14:17:52 -0600

    add table wait

--- manager/src/test/java/com/revature/end_to_end_tests/steps/ReviewExpenseSteps.java
+++ manager/src/test/java/com/revature/end_to_end_tests/steps/ReviewExpenseSteps.java
@@ -124,7 +124,7 @@ public class ReviewExpenseSteps {
         else
             expectedComment = comment;
         WebDriverWait wait = new WebDriverWait(context.getDriver(), java.time.Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("tbody")));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("#all-expenses-list tr")));
         List<WebElement> rows = context.getDriver().findElements(By.cssSelector("#all-expenses-list tr"));
         boolean found = false;
         // skip 1st row b/c it is the header
